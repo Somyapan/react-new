8bad834b2ee123754dc31565189d7abd9757a2c8
somya panchal <somyapancom@gmail.com>
2025-12-03 18:08:38 +0530

    new added

--- backend/wait-for-mysql.sh
+++ backend/wait-for-mysql.sh
@@ -1,9 +1,40 @@
+# #!/bin/sh
+
+# # Wait until MySQL is reachable
+# echo $MYSQL
+# while ! nc -z $MYSQL_HOST 3306; do
+#   echo "Waiting for MySQL..."
+#   sleep 3
+# done
+
+# # Then start the Node app
+# node server.js
+
+
+
+
 #!/bin/sh
-# Wait until MySQL is reachable
-while ! nc -z db 3306; do
-  echo "Waiting for MySQL..."
+
+#echo "Waiting for MySQL at $MYSQL_HOST:3306"
+
+#while ! (echo > /dev/tcp/$MYSQL_HOST/3306) >/dev/null 2>&1; do
+#  echo "MySQL not reachable..."
+#  sleep 3
+#done
+
+#echo "MySQL is reachable. Starting server..."
+#node server.js
+
+
+#!/bin/sh
+
+echo "Waiting for MySQL at $MYSQL_HOST:3306"
+
+# Loop until MySQL is reachable
+while ! nc -z "$MYSQL_HOST" 3306; do
+  echo "MySQL not reachable..."
   sleep 3
 done
 
-# Then start the Node app
+echo "MySQL is reachable. Starting server..."
 node server.js
